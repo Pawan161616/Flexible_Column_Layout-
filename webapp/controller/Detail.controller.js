0a8565f87be98f2f2b850444387edaca74ee0226
Pawan Khantwal <pawankhantwal16@gmail.com>
2022-07-25 16:34:54 +0000

    Routing done

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],function(Controller){
+    return Controller.extend("flexiblecollayout.controller.Detail",{
+         onInit: function(){
+            
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData({"Product":[]});
+            this.getView().setModel(oModel,"locModel");
+            
+             var oRouter = this.getOwnerComponent().getRouter();
+             oRouter.getRoute("RDetails").attachMatched(this._onObjectMatched.bind(this));
+         },
+         _onObjectMatched: function(oEvent){
+             var sId = oEvent.getParameters().arguments.ID;
+            var sPath = `/Products(${sId})`;
+            //  this.getView().bindElement({
+            //      path: sPath
+            //  });
+            var oDataModel = this.getView().getModel();
+            oDataModel.read(sPath,{success: function(oData,error){
+                
+                 var payload = {
+                     "ProductID":oData.ProductID,
+                     "ProductName":oData.ProductName,
+                     "QuantityPerUnit":oData.QuantityPerUnit,
+                     "UnitPrice":oData.UnitPrice
+                 };
+                 this.getView().getModel("locModel").setProperty("/Product",payload);
+            }.bind(this)
+        });
+
+         }
+    });
+});
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,Filter,FilterOperator) {
         "use strict";
 
         return Controller.extend("flexiblecollayout.controller.View1", {
@@ -19,9 +21,29 @@ sap.ui.define([
             },
 
 
-            onNav: function(){
+            onNav: function(event){
+
+                // to set app layout in two columns mid expanded layout when clicked on any list item 
               var oFCL = this.getView().getParent().getParent();
               oFCL.setLayout(sap.f.LayoutType.TwoColumnsMidExpanded);
+
+              var oSource = event.getSource();
+              var sPath = oSource.getBindingContextPath();
+              var ProductID = sPath.substr(sPath.length-2).charAt(0);
+              var oRouter = this.getOwnerComponent().getRouter();
+              oRouter.navTo("RDetails",{
+//ID is the name we pass as a paratmeter and same mentioned in manifest file
+                  ID: ProductID
+              });
+            },
+            onSearch: function(oEvent){
+                var oTable = this.getView().byId("IdProductList");
+               
+                var sQuery = oEvent.getParameter("query");
+                if(sQuery && sQuery.length>0){
+                   var oTableProductName = [new Filter("ProductName", FilterOperator.Contains,sQuery)];
+                }
+                oTable.getBinding("items").filter(oTableProductName);
             }
 
         });
